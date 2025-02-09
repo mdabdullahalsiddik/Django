430123364f7abfb824b69e7645979eed69c1913a
Md Abdullah Al Siddik <119064797+mdabdullahalsiddik@users.noreply.github.com>
2025-02-09 14:06:27 +0600

    templates

--- myapp/views.py
+++ myapp/views.py
@@ -13,4 +13,11 @@ def profile(request):
 def about (request):
     return render(request,template_name= 'about.html')
 def service (request):
-    return render(request,template_name= 'service.html')
+    data ={
+        "title" : "Service",
+        "services" : ["Service 1","Service 2","Service 3"],
+        "description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel ex at diam faucibus malesuada. Nulla facilisi. Sed euismod orci ac neque aliquam, vitae ultricies felis commodo. Sed consectetur, felis sit amet ullamcorper consectetur, lectus nunc lobortis arcu, in tristique felis ligula in nulla.",
+  
+        
+    }
+    return render(request,'service.html', data)
